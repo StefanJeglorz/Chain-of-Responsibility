74801dad9c6bba48bc34d68ea2c046ac7ba32b80
Stefan Jeglorz <S.Jeglorz@devware.de>
2024-01-26 16:48:35 +0100

    Added project main page

---
+++ Demo.Shared/DTOs/ProjectDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo.Shared.DTOs;
+
+public class ProjectDTO
+{
+    public ProjectDTO()
+    {
+        
+    }
+
+    public ProjectDTO(int id, string title, string description, int number) : this()
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Number = number;
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Number { get; set; }
+}
